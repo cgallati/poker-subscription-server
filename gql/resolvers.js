41720ef96d9da06fc2909ab983e4580a8ea53428
Chad Gallati <chad.gallati@somoglobal.com>
2022-05-12 16:46:55 -0400

    add basic room functions

--- gql/resolvers.js
+++ gql/resolvers.js
@@ -2,22 +2,42 @@ import {pubsub} from "../pubsub.js";
 
 let currentNumber = 0;
 
+let rooms = []
+
 export const resolvers = {
     Subscription: {
         counterChanged: {
             subscribe: () => pubsub.asyncIterator(["NUMBER_INCREMENTED"])
+        },
+        roomUpdated: {
+            subscribe: () => pubsub.asyncIterator(["ROOM_UPDATED"])
         }
     },
     Query: {
-        counter:
-            () => currentNumber
+        counter: () => currentNumber,
+        roomState: (id) => {
+            return rooms.find(room => room.id === id)
+        }
     },
     Mutation: {
         incrementCounter: () => {
             currentNumber++;
             return pubsub.publish('NUMBER_INCREMENTED', { counterChanged: currentNumber });
-        }
+        },
+        createRoom: (name) => {
+            const room = {
+                id: uuid.v1(),
+                name
+            }
+            rooms.push(room)
+            return room.id
+        },
+        updateName: (id, name) => {
+            const room = rooms.find(room => room.id === id)
+            room.name = name
+            return pubsub.publish('ROOM_UPDATED', {roomUpdated: room})
 
+        }
     }
 
 
--- gql/schema.js
+++ gql/schema.js
@@ -5,13 +5,23 @@ import {resolvers} from "./resolvers.js";
 const typeDefs = gql`
 type Subscription {
   counterChanged: Int
+  roomUpdated: RoomState
 }
 type Query {
   counter: Int
+  roomState(id: String):  RoomState
 }
 type Mutation {
     incrementCounter: Int 
+    createRoom(name: String): RoomState
+    updateName(id: String, name: String): RoomState
 }
+
+type RoomState {
+    id: String
+    name: String
+}
+
 `
 
 export const schema = makeExecutableSchema({
